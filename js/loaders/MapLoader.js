73c21f17c8c2366823627bb9e116c8b27d02c029
john <jgrayson@esri.com>
2022-01-06 17:59:25 -0800

    removed read-only aspect of map property in MapLoader

--- js/loaders/MapLoader.js
+++ js/loaders/MapLoader.js
@@ -1,51 +1,51 @@
 /*
-  Copyright 2020 Esri
+ Copyright 2020 Esri
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import Watchable from "../support/Watchable.js";
 
 class MapLoader extends Watchable {
-  
+
   // PORTAL ITEM ID //
   id;
   // MAP //
   map;
-  
+
   constructor(config) {
     super();
-    
+
     this.id = (config.webmap || config.webscene);
-    
+
     if (config.webmap) {
       require(['esri/WebMap'], WebMap => {
         this.map = new WebMap({portalItem: {id: this.id}});
       });
     }
-    
+
     if (config.webscene) {
       require(['esri/WebScene'], WebScene => {
         this.map = new WebScene({portalItem: {id: this.id}});
       });
     }
-    
+
     //
     // TODO: DO WE CREATE A DEFAULT MAP?
     //
-    
+
   }
-  
+
   loadMap() {
     return new Promise((resolve, reject) => {
       if (this.id) {
@@ -61,11 +61,11 @@ class MapLoader extends Watchable {
       }
     });
   }
-  
+
 }
 
 MapLoader.hasMap = (config) => {
-  return ((config.webmap != null) && config.webmap.length) || ((config.webscene != null) && config.webscene.length);
+  return (config.webmap?.length || config.webscene?.length);
 };
 
 export default MapLoader;
